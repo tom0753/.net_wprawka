3e57ac30122dfcc6d9017986841cf7b759b8e6cd
Tomasz <133196125+tom0753@users.noreply.github.com>
2026-03-23 19:30:41 +0000

    programcs

--- CoursesApp/Program.cs
+++ CoursesApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CoursesApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer("Server=localhost;Database=CoursesDb;Trusted_Connection=True;TrustServerCertificate=True"));
+
+var serviceProvider = services.BuildServiceProvider();
+
+// przykladowo
+using var scope = serviceProvider.CreateScope();
+var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+
